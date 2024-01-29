216c3510e4c10b5a753c158a5a19c23ab43312be
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-01-29 17:58:18 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio1DateTime
+{
+    /// <summary>
+    /// Clase principal de la aplicacion
+    /// JL - 29/01/2024
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Metodo principal de la aplicacion
+        /// JL - 29/01/2024
+        /// </summary>
+        static void Main(string[] args)
+        {
+            
+            DateTime fechaActual = DateTime.Now;
+            fechaActual.ToString("yy - MM - ddThh:mm: ss.ms");
+            Console.WriteLine("Fecha formato-tiempo: " + fechaActual.ToString());
+
+        }
+    }
+}
